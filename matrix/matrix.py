b5a5304fa53e430279ffde2c8690b8d04ae5e4ac
93fk <42356334+93fk@users.noreply.github.com>
2019-03-24 19:37:25 +0100

    Delete matrix.py

--- matrix/matrix.py
+++
@@ -1,9 +0,0 @@
-class Matrix(object):
-    def __init__(self, matrix_string):
-        self.matrix = [[int(number) for number in row.split(' ')] for row in matrix_string.split('\n')]
-
-    def row(self, index):
-        return self.matrix[index-1]
-
-    def column(self, index):
-        return [row[index-1] for row in self.matrix]
